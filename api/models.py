8943a05faa9dc9f131dbbbd7e0d84ca077ef9547
anthonynarine <anarine83@gmail.com>
2023-08-31 10:48:04 -0400

    serializers creagted + tested get and post request in postman, crating rate_movie method

--- api/models.py
+++ api/models.py
@@ -5,6 +5,9 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Movie(models.Model):
     title = models.CharField(max_length=32)
@@ -14,6 +17,10 @@ class Rating(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = (("user", "movie"),)
 
     class Meta:
         unique_together = (("user", "movie"),)
--- api/views.py
+++ api/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from .models import Movie, Rating
+from .serializers import MovieSerializer, RatingSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+    @action(detail=True, methods=["POST"])
+    def test_rate_movie(self, request, pk=None):
+        if "stars" in request.data:
+
+            
+
+
+            response = {"message": "test passed"}
+            return Response(response, status=status.HTTP_200_OK)
+        else:
+            response = {"message": "you need to provide stars"}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
