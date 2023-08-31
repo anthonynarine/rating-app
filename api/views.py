e2140a4f3a78909ff8ab893862046a37c15d66b8
anthonynarine <anarine83@gmail.com>
2023-08-31 16:59:11 -0400

    updated view to updated star rating or create a rating if rating does not exist

--- api/views.py
+++ api/views.py
@@ -1,24 +1,38 @@
-from django.shortcuts import render
+
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from .models import Movie, Rating
 from .serializers import MovieSerializer, RatingSerializer
 from rest_framework.response import Response
+from django.contrib.auth.models import User
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
-
+    
     @action(detail=True, methods=["POST"])
     def test_rate_movie(self, request, pk=None):
         if "stars" in request.data:
-
+            movie = Movie.objects.get(id=pk)
+            stars = request.data["stars"]
+            user = request.user
+            user = User.objects.get(id=1)
+            print(f"id is {pk}, movie tile, {movie.title}")
             
+            try:
+                rating = Rating.objects.get(user=user.id, movie=movie.id)
+                rating.stars = stars
+                rating.save()
+                serializer = RatingSerializer(rating, many=False)
+                response = {"message": "Rating updated", "result": serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+            except:
+                Rating.objects.create(user=user, movie=movie, stars=stars)
+                serializer = RatingSerializer(rating, many=False)
+                response = {"message": "Rating created", "result": serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
 
-
-            response = {"message": "test passed"}
-            return Response(response, status=status.HTTP_200_OK)
         else:
             response = {"message": "you need to provide stars"}
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
