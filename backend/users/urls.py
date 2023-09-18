f22c2bdd4bedea79d644d413723fb343e1a5fe04
anthonynarine <anarine83@gmail.com>
2023-09-18 00:31:25 -0400

    updated the urls in Users app and projects settings

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from rest_framework import routers
 from django.conf.urls import include
-from .views import MovieViewSet, RatingViewSet, UserViewSet
+from .views import UserViewSet
 
 
 router = routers.DefaultRouter()
-router.register("movies", MovieViewSet)
-router.register("ratings", RatingViewSet)
+router.register("users", UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
 
-# Create your views here.
+from re import U
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from .models import User
+from .schemas import user_list_docs
+from .serializers import UserSerializer
+
+class UserViewSet(viewsets.ViewSet):
+    queryset = User.objects.all()
+    
+    @user_list_docs
+    def list(self, request):
+        user_id = request.query_params.get("user_id")
+        queryset = User.objects.get(id=user_id)
+        serializer = UserSerializer(queryset)
+        return Response(serializer.data)
